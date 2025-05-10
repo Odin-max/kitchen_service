2aaa1fa2c6727b0d74eb0005ab1897121d87887a
Odin-max <meh84741@gmail.com>
2025-05-10 18:36:07 +0300

    staticfiles managing

---
+++ src/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+from str2bool import str2bool
+
+DEBUG = str2bool(os.environ.get("DEBUG"))
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
